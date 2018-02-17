ca98ba7c2d22ab7fadef8225b61878bd4b349391
Brendan Enrick <brendan@enrick.com>
2018-02-17 13:29:51 -0500

    Initial Project Skeleton

---
+++ src/UnitTests/TestsShould.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class TestsShould
+    {
+        [Test]
+        public void PassWhenEmpty()
+        {
+        }
+    }
+}
